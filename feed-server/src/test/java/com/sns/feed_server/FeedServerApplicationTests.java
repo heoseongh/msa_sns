cd9c6da4ce04accc9de1b3ad584b32961da9e013
heoseongh <shheo0807@gmail.com>
2024-10-02 15:26:25 +0900

    Init Infra Conifg

---
+++ feed-server/src/test/java/com/sns/feed_server/FeedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.feed_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
